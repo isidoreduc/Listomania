1bf9a96afeff42b1ab4593edc92c30063b1322b4
isidoreduc <isidoreducasse79@gmail.com>
2020-07-01 17:10:03 +0200

    create items

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,75 @@
 <template>
   <div id="app">
-    <h4 class="bg-primary text-white text-center p-3">{{name}}'s Listomania</h4>
+    <h4 class="bg-primary text-white text-center p-3">
+      {{ name }}'s Listomania
+    </h4>
     <div class="container-fluid p-4">
       <div class="row">
         <div class="col font-weight-bold">Tasks</div>
         <div class="col-2 font-weight-bold">Done</div>
       </div>
-      <div class="row" v-for = "t in filteredTasks" v-bind:key="t.action">
-        <div class="col">{{t.action}}</div>
+      <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
+        <div class="col">{{ t.action }}</div>
         <div class="col-2">
-          <input type="checkbox" v-model="t.done" class="form-check-input">
+          <input type="checkbox" v-model="t.done" class="form-check-input" />
+        </div>
+      </div>
+      <div class="row py-2 m-1">
+        <div class="col">
+          <input type="text" v-model="newItemText" class="form-check-input" />
+        </div>
+        <div class="col-2">
+          <button class="btn btn-primary" v-on:click="addNewItem">Add</button>
         </div>
       </div>
       <div class="row bg-secondary py-2 mt-2 text-white">
         <div class="col text-center">
-          <input type="checkbox"  v-model="hideCompleted" class="form-check-input" />
-          <label class="form-check-label font-weight-bold">Hide completed tasks</label>
+          <input
+            type="checkbox"
+            v-model="hideCompleted"
+            class="form-check-input"
+          />
+          <label class="form-check-label font-weight-bold"
+            >Hide completed tasks</label
+          >
         </div>
       </div>
+      
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "App",
-    data() {
-      return {
-        name : "Adrian",
-        tasks : [
-          {action: "Wet flowers", done: false},
-          {action: "Eat breakfast", done: true},
-          {action: "Sleep late", done: true},
-          {action: "Work", done: false},
-        ],
-        hideCompleted : true  
-      }
+export default {
+  name: 'App',
+  data() {
+    return {
+      name: 'Adrian',
+      tasks: [
+        { action: 'Wet flowers', done: false },
+        { action: 'Eat breakfast', done: true },
+        { action: 'Sleep late', done: true },
+        { action: 'Work', done: false },
+      ],
+      hideCompleted: true,
+      newItemText: '',
+    };
+  },
+  computed: {
+    filteredTasks() {
+      return this.hideCompleted
+        ? this.tasks.filter((t) => !t.done)
+        : this.tasks;
     },
-    computed : {
-      filteredTasks = () =>
-        this.hideCompleted ? 
-          this.tasks.filter(t => !t.done) : this.tasks 
-    }
-  };
+  },
+  methods: {
+    addNewItem() {
+      this.tasks.push({
+        action: this.newItemText,
+        done: false,
+      });
+      this.newItemText = '';
+    },
+  },
+};
 </script>
-
-
