1f2a5733a74de442d7f05809539259d2ce1fe45c
isidoreduc <isidoreducasse79@gmail.com>
2020-07-01 16:40:05 +0200

    filter list by completed

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,17 @@
         <div class="col font-weight-bold">Tasks</div>
         <div class="col-2 font-weight-bold">Done</div>
       </div>
-      <div class="row" v-for = "t in tasks" v-bind:key="t.action">
+      <div class="row" v-for = "t in filteredTasks" v-bind:key="t.action">
         <div class="col">{{t.action}}</div>
-        <div class="col-2">{{t.done}}</div>
+        <div class="col-2">
+          <input type="checkbox" v-model="t.done" class="form-check-input">
+        </div>
+      </div>
+      <div class="row bg-secondary py-2 mt-2 text-white">
+        <div class="col text-center">
+          <input type="checkbox"  v-model="hideCompleted" class="form-check-input" />
+          <label class="form-check-label font-weight-bold">Hide completed tasks</label>
+        </div>
       </div>
     </div>
   </div>
@@ -25,8 +33,14 @@
           {action: "Eat breakfast", done: true},
           {action: "Sleep late", done: true},
           {action: "Work", done: false},
-        ]
+        ],
+        hideCompleted : true  
       }
+    },
+    computed : {
+      filteredTasks = () =>
+        this.hideCompleted ? 
+          this.tasks.filter(t => !t.done) : this.tasks 
     }
   };
 </script>
