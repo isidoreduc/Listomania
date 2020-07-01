a93350a701b2f426044b8e5880fed84cc620e1f3
isidoreduc <isidoreducasse79@gmail.com>
2020-07-01 15:51:04 +0200

    display list

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,33 @@
 <template>
   <div id="app">
-    <h4 class="bg-primary text-white text-center p-3">Listmania</h4>
+    <h4 class="bg-primary text-white text-center p-3">{{name}}'s Listomania</h4>
+    <div class="container-fluid p-4">
+      <div class="row">
+        <div class="col font-weight-bold">Tasks</div>
+        <div class="col-2 font-weight-bold">Done</div>
+      </div>
+      <div class="row" v-for = "t in tasks" v-bind:key="t.action">
+        <div class="col">{{t.action}}</div>
+        <div class="col-2">{{t.done}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: "App"
+    name: "App",
+    data() {
+      return {
+        name : "Adrian",
+        tasks : [
+          {action: "Wet flowers", done: false},
+          {action: "Eat breakfast", done: true},
+          {action: "Sleep late", done: true},
+          {action: "Work", done: false},
+        ]
+      }
+    }
   };
 </script>
 
